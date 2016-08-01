63570b931ca2ea41a7b4ddbb6abfdc4bc4d5b706
Evan Waters <wakeboarder1335@gmail.com>
2016-08-01 16:20:25 -0600

    Remove error handling, return response as is

--- lib/synapse_pay_rest/http_client.rb
+++ lib/synapse_pay_rest/http_client.rb
@@ -21,7 +21,7 @@ module SynapsePayRest
         :accept => :json,
         'X-SP-GATEWAY' => gateway,
         'X-SP-USER' => user,
-        'X-SP-USER-IP' => config['ip_address'] 
+        'X-SP-USER-IP' => config['ip_address']
       }
     end
 
@@ -36,77 +36,25 @@ module SynapsePayRest
 
 
     def post(path, payload)
-      response = with_error_handling { RestClient.post(full_url(path), payload.to_json, get_headers()) }
-      JSON.parse(response)
+      RestClient.post(full_url(path), payload.to_json, get_headers())
     end
 
     def patch(path, payload)
-      response = with_error_handling { RestClient.patch(full_url(path), payload.to_json, get_headers()) }
-      JSON.parse(response)
+      RestClient.patch(full_url(path), payload.to_json, get_headers())
     end
 
     def get(path)
-      response = with_error_handling { RestClient.get(full_url(path), get_headers()) }
-      JSON.parse(response)
+      RestClient.get(full_url(path), get_headers())
     end
 
     def delete(path)
-      response = with_error_handling { RestClient.delete(full_url(path), get_headers()) }
-      JSON.parse(response)
+      RestClient.delete(full_url(path), get_headers())
     end
 
     private
 
     def full_url(path)
       "#{base_url}#{path}"
-    end
-
-    def with_error_handling
-      yield
-    rescue => e
-      # By the way, this is a really bad idea.
-      # See: https://www.relishapp.com/womply/ruby-style-guide/docs/exceptions
-      # The exceptions should be enumerated. Not all exceptions are going
-      # to be parsable by JSON. The only one that should be captured are the
-      # are the HTTP Client responses.
-      case e.response.code
-      when 400
-        return e.response
-      when 401
-        return e.response
-      when 409
-        return e.response
-      when 500
-        return e.response
-      when 405
-        return handle_method_not_allowed()
-      when 502
-        #Raise a gateway error
-        return handle_gateway_error()
-      when 504
-        #Raise a timeout error
-        return handle_timeout_error()
-      else
-        #Raise a generic error
-        return handle_unknown_error()
-      end
-    end
-
-    def handle_method_not_allowed()
-      return {'success' => false, 'reason' => 'The method is not allowed. Check your id parameters.'}.to_json
-    end
-
-    def handle_gateway_error()
-      return {'success' => false, 'reason' => 'The gateway appears to be down.  Check synapsepay.com or try again later.'}.to_json
-    end
-
-    def handle_timeout_error()
-      return {'success' => false, 'reason' => 'A timeout has occurred.'}.to_json
-    end
-
-
-    def handle_unknown_error()
-      return {'success' => false, 'reason' => 'Unknown error in library. Contact synapsepay.'}.to_json
     end
   end
 end
